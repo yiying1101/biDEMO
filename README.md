3de1e50514595aa5eb19fd243270d55727d3e4fc
yiying1101 <leeying11011101@gmail.com>
2025-05-25 13:46:40 +0800

    bmi xiangmu

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # biDEMO
+=======
+# bmiDEMO
+>>>>>>> 214ed91 (bmi xiangmu)
 欢迎来到这个转换计算机
--- chazhang.py
+++ chazhang.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 print("欢迎使用简易计算器")
 num1 = float(input("请输入第一个数字："))
 op = input("请选择运算符 (+ - * /)：")
@@ -18,3 +19,25 @@ else:
     result = "无效的运算符"
 
 print("结果是：", result)
+=======
+print("欢迎使用简易计算器")
+num1 = float(input("请输入第一个数字："))
+op = input("请选择运算符 (+ - * /)：")
+num2 = float(input("请输入第二个数字："))
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    result = num1 -num2
+elif op == "*":
+    result = num1 * num2
+elif op == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "除数不能为0"
+else:
+    result = "无效的运算符"
+
+print("结果是：", result)
+214ed91 (bmi xiangmu)
